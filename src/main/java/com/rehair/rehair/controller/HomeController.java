76d8bf2a3edb9059192ae2777dc20583e1ca38b3
Kang hyungoo <hyungoo7703@gmail.com>
2021-12-11 12:29:40 +0900

    1차 frontend 코드머지 수정사항 수정예정

---
+++ src/main/java/com/rehair/rehair/controller/HomeController.java
@@ -0,0 +1,29 @@
+package com.rehair.rehair.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/reservation")
+    public String reservation() {
+        return "reservation";
+    }
+
+    @GetMapping("/event")
+    public String event() {
+        return "event";
+    }
+
+}
