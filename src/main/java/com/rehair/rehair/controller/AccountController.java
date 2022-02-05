a7714f994d0ea484a7930f8b0c3a9cea5e4d9b3e
kyungnan <baekn1023@google.com>
2022-02-05 11:28:25 +0900

    회원가입 구현 및 about, reservation_check 수정

--- src/main/java/com/rehair/rehair/controller/AccountController.java
+++ src/main/java/com/rehair/rehair/controller/AccountController.java
@@ -1,20 +1,46 @@
 package com.rehair.rehair.controller;
 
+import com.rehair.rehair.domain.User;
+import com.rehair.rehair.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/account")
 public class AccountController {
 
+	@Autowired
+	private final UserService userService;
+
 	@GetMapping("/login")
-	public String login() {
+	public String login(@RequestParam(required = false) boolean isSuccess, Model model) {
+		if (isSuccess){
+			model.addAttribute("isSuccess", true);
+		}
 		return "account/login";
 	}
 
 	@GetMapping("/join")
-	public String join() {
+	public String joinForm() {
 		return "account/join";
+	}
+
+	@PostMapping("/join")
+	public String join(User user, Model model, RedirectAttributes redirectAttributes){
+		if ((ObjectUtils.isEmpty(user.getUsername())) || ObjectUtils.isEmpty(user.getPassword()) || ObjectUtils.isEmpty(user.getName()) || ObjectUtils.isEmpty(user.getPhoneNumber()) || ObjectUtils.isEmpty(user.getBirth()) || ObjectUtils.isEmpty(user.getGender())){
+			String msg = "회원정보에 누락된 곳이 있습니다. 확인 후 기입하시기 바랍니다.";
+			model.addAttribute("msg", msg);
+			return "account/join";
+		}
+
+		User newUser = userService.joinUser(user);
+		redirectAttributes.addFlashAttribute("isSuccess", true);
+		return "redirect:/account/login";
 	}
 }
