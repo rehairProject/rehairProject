fecfec7d21c59893143b129519a49e55667d8676
Kang Hyungoo <hyungoo7703@gmail.com>
2022-02-09 13:49:46 +0900

    Notice Crud 구현, 이미지 파일 png 변경(로딩속도 개선) 앞으로 필요 요구사항(개선사항) 1. 로그인 후 화면 권환에 따라 나누어 처리 2. CSS 처리 3. 자바스크립트 검증로직 추가 4. EVENT와 소스 통일

--- src/main/java/com/rehair/rehair/controller/ClientController.java
+++ src/main/java/com/rehair/rehair/controller/ClientController.java
@@ -1,12 +1,17 @@
 package com.rehair.rehair.controller;
 
 import com.rehair.rehair.domain.Event;
+import com.rehair.rehair.domain.Notice;
 import com.rehair.rehair.repository.EventRepository;
+import com.rehair.rehair.repository.NoticeRepository;
 import com.rehair.rehair.service.EventService;
 import com.rehair.rehair.validator.EventValidator;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.EntityManager;
+
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -15,90 +20,127 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.List;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/client")
+@RequiredArgsConstructor
 public class ClientController {
-    @Autowired
-    private EventService eventService;
-    @Autowired
-    private EventRepository eventRepository;
-    @Autowired
-    private EventValidator eventValidator;
 
+	private final NoticeRepository noticeRepository;
+	private final EventService eventService;
+	private final EventRepository eventRepository;
+	private final EventValidator eventValidator;
 
-    @GetMapping("/about")
-    public String about() {
-        return "client/about";
+	@GetMapping("/about")
+	public String about() {
+		return "client/about";
+	}
+
+	@GetMapping("/reservation")
+	public String reservation() {
+		return "client/reservation";
+	}
+	
+	@GetMapping("/reservation_check")
+	public String reservationCheck() {
+		return "client/reservation_check";
+	}
+
+	// == Notice 로직 시작 ==//
+
+	@GetMapping("/notice")
+	public String notices(Model model,
+			@PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+		Page<Notice> notices = noticeRepository.findAll(pageable);
+		int startPage = Math.max(1, notices.getPageable().getPageNumber() - 4);
+		int endPage = Math.min(notices.getTotalPages(), notices.getPageable().getPageNumber() + 4);
+
+		model.addAttribute("indexCalculator", notices.getTotalElements() - notices.getPageable().getPageNumber() * 10);
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
+		model.addAttribute("notices", notices);
+		return "client/notice";
+	}
+
+	@GetMapping("/notice_detail")
+    public String notice(Model model, @RequestParam(required = false) Long id) {
+    	Notice notice = noticeRepository.findById(id).orElse(null);
+		model.addAttribute("notice", notice);
+    	return "client/notice_detail";
     }
+	
+	@GetMapping("/notice_delete") 
+	public String noticeDelete(@RequestParam Long id, RedirectAttributes redirectAttributes) {
+	    noticeRepository.deleteById(id);
+		redirectAttributes.addAttribute("deleteStatus", true); // 상태 전송
+		return "redirect:/client/notice";
+	}
 
-    @GetMapping("/reservation")
-    public String reservation() {
-        return "client/reservation";
-    }
+	@GetMapping("/notice_writing") // 입력, 수정 폼 공유
+	public String noticeWriting(Model model, @RequestParam(required = false) Long id) {
+		if (id == null)
+			model.addAttribute("notice", new Notice());
+		else {
+			Notice notice = noticeRepository.findById(id).orElse(null);
+			model.addAttribute("notice", notice);
+		}
+		return "client/notice_writing";
+	}
 
-    @GetMapping("/notice")
-    public String notice(Model model, @PageableDefault(size = 3, sort = "id",  direction = Sort.Direction.DESC)Pageable pageable) {
-//        Page<Event> events = eventRepository.findAll(pageable);
-//        int startPage=Math.max(1,events.getPageable().getPageNumber()-4);
-//        int endPage = Math.min(events.getTotalPages(), events.getPageable().getPageNumber() + 4);
-//
-//        model.addAttribute("indexCalculator", events.getTotalElements() - events.getPageable().getPageNumber() * 3);
-//
-//        model.addAttribute("startPage",startPage);
-//        model.addAttribute("endPage", endPage);
-//        model.addAttribute("events", events);
-        return "client/notice";
-    }
+	@PostMapping("/notice_writing")
+	public String noticeSubmit(Notice notice, RedirectAttributes redirectAttributes) {
+		noticeRepository.save(notice);
 
-    @GetMapping("/reservation_check")
-    public String reservationCheck() {
-        return "client/reservation_check";
-    }
+		redirectAttributes.addAttribute("notice", notice); // model.addAttribute("notice", notice); 동일
+		redirectAttributes.addAttribute("writeStatus", true); // 상태 전송
+		return "redirect:/client/notice";
+	}
 
-    @GetMapping("/event_detail")
-    public String eventDetail(Model model, @RequestParam(required = false) Long id) {
-        Event event = eventRepository.findById(id).orElse(null);
-        model.addAttribute("event", event);
-        return "client/event_detail";
-    }
+	// == Notice 로직 끝 ==//
 
-    @GetMapping("/event_writing")
-    public String eventWriting(Model model, @RequestParam(required = false) Long id) {
-        if(id==null){
-            model.addAttribute("event", new Event());
-        }else{
-            Event event = eventRepository.findById(id).orElse(null);
-            model.addAttribute("event", event);
-        }
 
-        return "client/event_writing";
-    }
+	@GetMapping("/event_detail")
+	public String eventDetail(Model model, @RequestParam(required = false) Long id) {
+		Event event = eventRepository.findById(id).orElse(null);
+		model.addAttribute("event", event);
+		return "client/event_detail";
+	}
 
-    @PostMapping("/event_writing")
-    public String eventSubmit( Event event, BindingResult bindingResult, MultipartFile file) throws Exception {
-        eventValidator.validate(event,bindingResult);
-        if (bindingResult.hasErrors()) {
-            return "client/event_writing";
-        }
-        eventService.save(event, file);
-        return "redirect:/client/notice";
-    }
+	@GetMapping("/event_writing")
+	public String eventWriting(Model model, @RequestParam(required = false) Long id) {
+		if (id == null) {
+			model.addAttribute("event", new Event());
+		} else {
+			Event event = eventRepository.findById(id).orElse(null);
+			model.addAttribute("event", event);
+		}
 
-    @GetMapping("/event")
-    public String event(Model model, @PageableDefault(size = 3, sort = "id",  direction = Sort.Direction.DESC)Pageable pageable) {
-        Page<Event> events = eventRepository.findAll(pageable);
-        int startPage=Math.max(1,events.getPageable().getPageNumber()-4);
-        int endPage = Math.min(events.getTotalPages(), events.getPageable().getPageNumber() + 4);
+		return "client/event_writing";
+	}
 
-        model.addAttribute("indexCalculator", events.getTotalElements() - events.getPageable().getPageNumber() * 3);
+	@PostMapping("/event_writing")
+	public String eventSubmit(Event event, BindingResult bindingResult, MultipartFile file) throws Exception {
+		eventValidator.validate(event, bindingResult);
+		if (bindingResult.hasErrors()) {
+			return "client/event_writing";
+		}
+		eventService.save(event, file);
+		return "redirect:/client/notice";
+	}
 
-        model.addAttribute("startPage",startPage);
-        model.addAttribute("endPage", endPage);
-        model.addAttribute("events", events);
-        return "client/event";
-    }
+	@GetMapping("/event")
+	public String event(Model model,
+			@PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+		Page<Event> events = eventRepository.findAll(pageable);
+		int startPage = Math.max(1, events.getPageable().getPageNumber() - 4);
+		int endPage = Math.min(events.getTotalPages(), events.getPageable().getPageNumber() + 4);
+
+		model.addAttribute("indexCalculator", events.getTotalElements() - events.getPageable().getPageNumber() * 3);
+
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
+		model.addAttribute("events", events);
+		return "client/event";
+	}
 }
---
+++ src/main/java/com/rehair/rehair/repository/NoticeRepository.java
@@ -0,0 +1,9 @@
+package com.rehair.rehair.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rehair.rehair.domain.Notice;
+
+public interface NoticeRepository extends JpaRepository<Notice, Long>{
+
+}
