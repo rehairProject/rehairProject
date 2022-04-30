339d866776ae084d59f64c3c6abd3a9c4a0211b2
kyungnan <baekn1023@google.com>
2022-04-30 14:44:39 +0900

    관리자 페이지 추가

--- src/main/java/com/rehair/rehair/controller/HomeController.java
+++ src/main/java/com/rehair/rehair/controller/HomeController.java
@@ -1,14 +1,55 @@
 package com.rehair.rehair.controller;
 
+import com.rehair.rehair.domain.HolidayStatus;
+import com.rehair.rehair.domain.Schedule;
+import com.rehair.rehair.repository.ScheduleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final ScheduleRepository scheduleRepository;
 
     @GetMapping("/")
     public String home() {
         return "home";
     }
 
+    @GetMapping("/admin")
+    public String admin(Model model){
+        List<Schedule> schedules = scheduleRepository.findAll();
+        model.addAttribute("schedules", schedules);
+        return "admin";
+    }
+
+    @PostMapping("/admin/holiday")
+    public String holiday(@RequestParam String date, @RequestParam String designer){
+        //DB에 휴무 직원 저장
+        Schedule schedule = scheduleRepository.getById(date);
+        schedule.setStatus(HolidayStatus.nameOf(designer));
+        scheduleRepository.save(schedule);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("admin/holiday/{date}")
+    public @ResponseBody String holidayDesigner(@PathVariable String date){
+        Schedule schedule = scheduleRepository.getById(date);
+        String holidayDesigner;
+        if(ObjectUtils.isEmpty(schedule.getStatus())){
+            holidayDesigner = "";
+        } else {
+            HolidayStatus holidayStatus = schedule.getStatus();
+            holidayDesigner = holidayStatus.getName();
+        }
+        return holidayDesigner;
+    }
 }
---
+++ src/main/resources/static/js/admin.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+//탭 버튼
+    $(".tab_box > .re_btn").click(function() {
+        var idx = $(this).index();
+        $(".tab_box > .re_btn").removeClass("on");
+        $(".tab_box > .re_btn").eq(idx).addClass("on");
+        $(".mainWrap > .tab:not(:eq(idx))").hide();
+        $(".mainWrap > .tab").eq(idx).show();
+    });
+
+//datepicker
+    var tomorrowDate = tomorrowDate();
+    $('#calendar').datepicker({
+        dateFormat: 'yy-mm-dd',
+		dayNamesMin : ['일','월','화','수','목','금','토'],
+		monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+		minDate: new Date(tomorrowDate),
+		beforeShowDay: function(date){
+            return [(date.getDay() != 0)];
+        },
+		onSelect: function(dateText, inst) {
+            var date = $(this).val();
+            alert('선택하신 날짜는'+date);
+            $('#date').val(date);
+
+            $.ajax({
+                type:"GET",
+                url:`admin/holiday/${date}`,
+            }).done(function(resp){
+                $('#holidayDesigner').text(resp);
+            }).fail(function(error){
+                console.log(JSON.stringify(error));
+            });
+        }
+	});
+    function tomorrowDate(){
+        var tomorrowDate ="";
+        var today = new Date();
+        var dd = today.getDate()+1;
+        var mm = today.getMonth()+1;
+        var yyyy = today.getFullYear();
+        if(dd<10) {
+            dd='0'+dd
+        }
+        if(mm<10) {
+            mm='0'+mm
+        }
+        tomorrowDate = yyyy+'-'+mm+'-'+dd;
+        return tomorrowDate;
+    }
+
+//휴무정보 유효성검사
+    $("#nextBtn").on("click", function(){
+        //날짜
+        var date = $("#date").val();
+        if (date == "null"){
+            alert("날짜를 선택하세요.");
+            return;
+        }
+        $("#date").val(date);
+
+        //디자이너
+        var designer = $("input[name='designer']:checked").val();
+        if (designer == null){
+            alert("디자이너를 선택하세요.");
+            return;
+        }
+        $("#reservationDesigner").val(designer);
+        $("#holidayInfo").submit();
+    });
+});
+
+//예약관리
+function confirmmation() {
+    confirm("금일 09:30 김철수님 방문 확정하시겠습니까?");
+}
